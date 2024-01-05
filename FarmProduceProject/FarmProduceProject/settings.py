8895db078fe8064ac1154fe7448755b17900da41
Sonnie <mgathoni.gathoni9@gmail.com>
2024-01-05 15:20:46 +0300

    final commit

--- FarmProduceProject/FarmProduceProject/settings.py
+++ FarmProduceProject/FarmProduceProject/settings.py
@@ -123,4 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-MODELS = os.path.join(Ml_models, 'ML_models/plant_xgb_model.pkl')
+MODELS = os.path.join('', 'ML_models/plant_xgb_model.pkl')
