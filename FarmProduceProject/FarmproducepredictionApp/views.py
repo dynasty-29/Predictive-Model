f4f69c94f26477daca80d93b687ebadda9ccb1cf
Sonnie <mgathoni.gathoni9@gmail.com>
2024-01-08 05:05:44 +0300

    updates

--- FarmProduceProject/FarmproducepredictionApp/views.py
+++ FarmProduceProject/FarmproducepredictionApp/views.py
@@ -4,20 +4,24 @@ from .models import CropPrediction
 import numpy as np
 from .apps import *
 from rest_framework.views import APIView
-from rest_framework.response import  Response
+from rest_framework.response import Response
+from sklearn.preprocessing import LabelEncoder
+
+le = LabelEncoder()
+
 
 class CropPrediction(APIView):
     def post(self, request, format=None):
         model = FarmproducepredictionappConfig.model
-        temperature = request.data['temperature']
-        humidity = request.data['humidity']
-        precipitation = request.data['precipitation']
-        wind_speed = request.data['wind_speed']
-        solar_radiation = request.data['solar_radiation']
-        nitrogen_level = request.data['nitrogen_level']
-        phosphorus_level = request.data['phosphorus_level']
-        potassium_level = request.data['potassium_level']
-        ph_level = request.data['ph_level']
+        temperature = request.data["temperature"]
+        humidity = request.data["humidity"]
+        precipitation = request.data["precipitation"]
+        wind_speed = request.data["wind_speed"]
+        solar_radiation = request.data["solar_radiation"]
+        nitrogen_level = request.data["nitrogen_level"]
+        phosphorus_level = request.data["phosphorus_level"]
+        potassium_level = request.data["potassium_level"]
+        ph_level = request.data["ph_level"]
 
         lis = []
 
@@ -35,5 +39,7 @@ class CropPrediction(APIView):
 
         classification = model.predict([lis])
 
-        return Response({'classification_result': classification[0]}, status=200)
+        # Convert the numeric prediction back to the original crop type name
+        prediction_name = le.inverse_transform([classification[0]])[0]
 
+        return Response({"classification_result": prediction_name}, status=200)
